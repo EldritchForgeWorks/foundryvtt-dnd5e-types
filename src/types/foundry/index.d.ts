68fdeca553ca8c9e0a47fa0905e083a2624fd0fa
JC Murray <{ID}+{username}@users.noreply.github.com>
2025-04-19 21:39:23 -0600

    fix: correct import paths in foundry/index.d.ts
    This commit fixes the incorrect import paths in the foundry/index.d.ts file:
    
    1. Update Application class path to point to client/apps/app
    2. Update ActorSheet and ItemSheet paths to point to client/apps/forms
    3. Update document class paths to point to client/data/documents
    4. Update data type paths to match their document class paths
    
    These changes ensure that the re-exported Foundry VTT types correctly
    reference the actual paths in the @league-of-foundry-developers/foundry-vtt-types
    package, fixing inheritance issues when consuming modules import our types.

--- src/types/foundry/index.d.ts
+++ src/types/foundry/index.d.ts
@@ -3,23 +3,21 @@
  */
 
 // Applications
-export { ItemSheet } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/applications/item-sheet';
-export { ActorSheet } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/applications/actor-sheet';
-export { Application } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/applications/application';
+export { Application } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/apps/app';
+export { ActorSheet } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/apps/forms/actor';
+export { ItemSheet } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/apps/forms/item';
 
 // Documents
-export { Actor } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/actor';
-export { Item } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/item';
-export { Token } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/token';
-export { ChatMessage } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/chat-message';
-export {
-  Combat,
-  Combatant,
-} from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/combat';
-export { ActiveEffect } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/active-effect';
-export { JournalEntryPage } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/journal-entry';
+export { Actor } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/actor';
+export { Item } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/item';
+export { Token } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/token';
+export { ChatMessage } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/chat-message';
+export { Combat } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/combat';
+export { Combatant } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/combatant';
+export { ActiveEffect } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/active-effect';
+export { JournalEntryPage } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/journal-entry';
 
 // Data types
-export { ActorData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/actor';
-export { ItemData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/item';
-export { TokenData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/documents/token';
+export { ActorData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/actor';
+export { ItemData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/item';
+export { TokenData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/documents/token';
